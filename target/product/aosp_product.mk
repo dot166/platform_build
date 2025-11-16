0d83c7c3a980d5e33b68e9321df5802cbb1c1496
._______166 <retromac9x2@gmail.com>
2025-11-16 23:40:57 +0900

    change default sounds

--- target/product/aosp_product.mk
+++ target/product/aosp_product.mk
@@ -23,8 +23,8 @@ $(call inherit-product-if-exists, frameworks/base/data/sounds/AllAudio.mk)
 
 # Additional settings used in all AOSP builds
 PRODUCT_PRODUCT_PROPERTIES += \
-    ro.config.ringtone?=Ring_Synth_04.ogg \
-    ro.config.notification_sound?=pixiedust.ogg \
+    ro.config.ringtone?=Orion.ogg \
+    ro.config.notification_sound?=Argon.ogg \
     ro.com.android.dataroaming?=true \
     ro.boot.vendor.overlay.theme=com.android.internal.systemui.navbar.gestural
 
